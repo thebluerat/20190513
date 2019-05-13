08072dc68b887b3d92bb4a884c86c78d36c4fabf
GD3 <GD3@DESKTOP-TQ15C6N>
2019-05-13 10:51:01 +0900

    구구단 끝

--- src/java0513/Times.java
+++ src/java0513/Times.java
@@ -1,10 +1,42 @@
 package java0513;
 
-public class Times {
+ public class Times
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+/*	public static void main(String[] args) 
+	{
+		for(int i = 1; i < 10; i++)
+		{System.out.println(i + "단");
+			for(int j = 1; j < 10; j++)
+			{
+				System.out.println(i + " * " + j + " = " + i*j);
+			}System.out.println();
+		}
+	}*/
+	
+/*	public static void main(String[] args)
+	{
+		for(int i = 1; i <10; i+=3)
+		{
+			System.out.println(i + "단\t\t" + (i+1) + "단\t\t" + (i+2) + "단\t\t");
+			for(int j = 1; j <10; j++)
+			{
+				System.out.println(i + " * " + j + " = " + i*j + "\t" + (i+1) + " * " + j + " = " + (i+1)*j + "\t" + (i+2) + " * " + j + " = " + (i+2)*j);
+			}System.out.println();
+			
+		}
+	}*/
+	 
+	 public static void main(String[] args)
+	 {
+		 for(int i = 1; i < 4; i++)
+		 {
+			 System.out.println(i + "단\t\t" + (i+3) + "단\t\t" + (i+6) + "단\t\t");
+			 for(int j = 1; j <10; j++)
+			 {
+				 System.out.println(i + " * " + j + " = " + i*j + "\t" + (i+3) + " * " + j + " = " + (i+3)*j + "\t" + (i+6) + " * " + j + " = " + (i+6)*j);
+			 }System.out.println();
+		 }
+	 }
 
 }
